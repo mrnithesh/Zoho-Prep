6c75ff5e34a22b6fd25349380999834763817171
mrnithesh <mr.nithesh.k@gmail.com>
2025-09-20 20:03:01 +0530

    Add class to find the length of the longest substring without repeating characters

---
+++ src/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class LongestSubstringWithoutRepeatingCharacters {
+    public static void main(String[] args) {
+        String str ="abcabcdec";
+        System.out.println(lengthOfLongestSubstring(str));
+    }
+    public static int lengthOfLongestSubstring(String s) {
+        int l = 0;
+        int res = 0;
+        HashSet<Character> set = new HashSet<>();
+        for (int r=0;r<s.length();r++){
+            char ch = s.charAt(r);
+            while (set.contains(ch)){
+                set.remove(s.charAt(l));
+                l++;
+            }
+            set.add(s.charAt(r));
+            res = Math.max(res,r-l+1);
+        }
+        return res;
+    }
+}
